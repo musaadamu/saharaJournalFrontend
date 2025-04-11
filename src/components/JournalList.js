b6b8533da244597cabe5dbc35e97a886f82a2fbc
Musa Adamu <msmajemusa4@gmail.com>
2025-04-11 15:46:40 +0100

    buttons

--- src/components/JournalList.js
+++ src/components/JournalList.js
@@ -163,7 +163,7 @@ const JournalList = () => {
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
                                             <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
                                         </svg>
-                                        PDF
+                                        Download PDF
                                     </button>
                                     <button
                                         onClick={() => handleDownload(journal._id, 'docx')}
@@ -172,7 +172,7 @@ const JournalList = () => {
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
                                             <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
                                         </svg>
-                                        DOCX
+                                        Download DOCX
                                     </button>
                                 </div>
                             </div>
